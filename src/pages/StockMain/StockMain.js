99b7ad3a7456b4825ffdf5e3c299c75e2f46f1fa
wolf <palanivaradharaj7@gmail.com>
2022-03-12 20:22:35 +0530

    responsive-graph

--- src/pages/StockMain/StockMain.js
+++ src/pages/StockMain/StockMain.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import styles from "./StockMain.module.css"
+import Graph from "./Graph";
+import Button from './../../components/Button/Button';
+
+
 
 function StockMain() {
+
+  function onclk() {
+    window.open('/');
+  }
+
   return (
-    <div>StockMain</div>
+    <div className={`${styles.container}`}>
+      <Graph />
+      <div className={`${styles.btns}`}>
+        <div className={`${styles.lbtn}`}>
+          <Button text='Buy' onClickMethod={onclk} />
+        </div>
+        <div className={`${styles.rbtn}`}>
+          <Button text='Sell' onClickMethod={onclk} />
+        </div>
+      </div>
+    </div>
   )
 }
 
