0be4c9a59052315823cca9bd5033dac49ad64bc0
Vaalarivan2002 <pvaalarivan@gmail.com>
2022-03-17 00:28:40 +0530

    Added Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from "./Button.module.css"
 
-function Button() {
+function Button({ text, onClickMethod, color = "rgb(0, 156, 222)" }) {
   return (
-    <div>Button</div>
-  )
+    <button className={`${styles.button}`} onClick={onClickMethod} style={{ backgroundColor: color }}>
+      {text}
+    </button>
+  );
 }
 
 export default Button
