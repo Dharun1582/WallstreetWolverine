99bf18b3dcc103899f474859b82f4a374b85d67a
Vaalarivan2002 <pvaalarivan@gmail.com>
2022-03-17 01:21:30 +0530

    register-components

---
+++ src/data/RegisterDetails.js
@@ -0,0 +1,55 @@
+export const REGISTER_FORM_FIELDS = [
+    {
+        type: "text",
+        heading: "Name",
+        name: "name",
+    },
+    {
+        type: "text",
+        heading: "K! ID",
+        name: "kId"
+    },
+    {
+        type: "tel",
+        heading: "Contact number",
+        name: "number"
+    },
+    {
+        type: "text",  // maybe change type to email later
+        heading: "Email ID",
+        name: "email",
+    },
+    {
+        type: "text",
+        heading: "College name",
+        name: "college"
+    },
+    {
+        type: "text",
+        heading: "Department",
+        name: "department"
+    },
+    {
+        type: "password",
+        heading: "Password",
+        name: "password"
+    },
+    {
+        type: "password",
+        heading: "Confirm Password",
+        name: "confirmPassword"
+    },
+];
+
+export const LOGIN_FORM_FIELDS = [
+    {
+        type: "text",
+        heading: "K! ID",
+        name: "kId"
+    },
+    {
+        type: "password",
+        heading: "Password",
+        name: "password"
+    },
+]
