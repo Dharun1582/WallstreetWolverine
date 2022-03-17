94a9ee6190a204f03b4f4991ed021b54d85596c2
Dharun <dharun1582@gmail.com>
2022-03-17 22:00:41 +0530

    market

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,73 @@
+import { Container } from '@mui/material'
 import React from 'react'
-import styles from "./Navbar.module.css"
+// import styles from './Navbar.module.css'
+import "./navbar.css"
+
+
+
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+      <div>
+
+
+		          
+		{/* <div className={`${styles.nav}`}>
+  <input type="checkbox" id="nav-check" />
+  <div className={`${styles.nav_header}`}>
+    <div className={`${styles.nav_title}`}>
+		<img className={`${styles.logo}`} src='images/logos/logo.png' />
+      
+    </div>
+  </div>
+  <div className={`${styles.nav_btn}`}>
+    <label htmlFor="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+  <div className={`${styles.nav_links}`}>
+    <a href="//github.io/jo_geek" target="_blank">Github</a>
+    <a href="/market" target="_blank">Market</a>
+    <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">LinkedIn</a>
+    <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
+    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a>
+  </div>
+</div> */}
+
+         
+		<div className="nav">
+  <input type="checkbox" id="nav-check" />
+  <div className="nav-header">
+    <div className="nav-title">
+		<img className='logo' src='../images/logos/logo.png' />
+      
+    </div>
+  </div>
+  <div className="nav-btn">
+    <label htmlFor="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+  <div className="nav-links">
+    <a href="//github.io/jo_geek" target="_blank">Github</a>
+    <a href="/market" target="_blank">Market</a>
+    <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">LinkedIn</a>
+    <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
+    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a>
+  </div>
+</div> 
+
+
+      </div>
+
+
   )
 }
 
--- src/pages/Market/Market.js
+++ src/pages/Market/Market.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from "./Market.module.css"
 import {Link} from 'react-router-dom';
 import companies from "./companies.json"
-
+import NewsFeed from '../../components/NewsFeed/NewsFeed';
 
 
 function Header(){
@@ -18,7 +18,6 @@ function Card(props){
   return(
     
     <a href={`/stock/${props.name}`}>
-    
       <div className={`${styles.card}`}>
       <img src='images/logos/filler.jpg' alt=''/>
       <h3>{props.name}</h3>
@@ -46,12 +45,31 @@ function CardList(){
 
 
 function Market() {
+
+  var nrows;
+  console.log(window.innerWidth);
+  if(window.innerWidth > 1024){
+    nrows=7;
+  }else{
+    nrows=2;
+  }
+
   return (
     <>
       <Header />
-      <div className={`${styles.container}`}>
-        <CardList />
+      <div className={`${styles.uppercontainer}`}>
+
+        <div className={`${styles.item}`}>
+          <NewsFeed nrows={nrows} />
+        </div>
+        <div>
+          <h2 className={`${styles.stocksheading}`}>Stocks</h2><br /> 
+        <div className={`${styles.container}`}>
+          <CardList />
+        </div>
+        </div>
       </div>
+      
     </>
 
   )
