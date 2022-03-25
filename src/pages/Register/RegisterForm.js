4f751e31e63426b505970779602d8638a3b32f67
Vaalarivan2002 <pvaalarivan@gmail.com>
2022-03-25 23:13:06 +0530

    fixed login popup glitch

--- src/pages/Register/RegisterForm.js
+++ src/pages/Register/RegisterForm.js
@@ -2,19 +2,18 @@ import React, { useState, useRef } from "react";
 import Button from "../../components/Button/Button";
 import FormField from "../../components/FormField/FormField";
 import { LOGIN_FORM_FIELDS } from "../../data/RegisterDetails";
-import styles from "./Register.module.css"; 
+import styles from "./Register.module.css";
 import ReCAPTCHA from "react-google-recaptcha";
 import SimpleLoader from "../../components/SimpleLoader/SimpleLoader";
 import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
 import Heading from "../../components/Heading/Heading";
 import Popup from "../../components/Popup/Popup";
-import { validateForm  } from "../../validators/registerFormValidator";
 import { ReactNotifications, Store } from 'react-notifications-component'
 import 'react-notifications-component/dist/theme.css'
 import "animate.css"
 import { useNavigate } from "react-router-dom";
-const axios=require('axios');
+const axios = require('axios');
 
 const loginDetailsFormat = {
     email: "",
@@ -24,25 +23,39 @@ const loginDetailsFormat = {
 const k_api = axios.create({
     // baseURL: "http://localhost:3001/",
     baseURL: "https://api.kurukshetraceg.org.in/",
-  });
-  
-  const url_login = "api/user/login";
-  
-  const apiLogin = async (data) => {
+});
+
+const url_login = "api/user/login";
+
+const apiLogin = async (data) => {
     try {
-      const response = await k_api.post(`${url_login}`, data);
-      return response;
+        const response = await k_api.post(`${url_login}`, data);
+        return response;
     } catch (error) {
-      return error.response;
+        return error.response;
     }
-  };
+};
+
+const showMessage = (title, type) => {
+    Store.addNotification({
+        title: title,
+        type: type,
+        insert: "bottom",
+        container: "top-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+            duration: 3000,
+            onScreen: true
+        }
+    })
+}
 
 function RegisterForm() {
     const navigate = useNavigate()
+    let reCaptchaRef = useRef(null);
     const [checked, setChecked] = useState(false);
     const [isDisabled, setIsDisabled] = useState(true);
-    let reCaptchaRef = useRef(null);
-
     // const [registerDetails, setRegisterDetails] = useState(registerDetailsFormat);
     const [loginDetails, setLoginDetails] = useState(loginDetailsFormat);
     const [isModalOpen, setIsModalOpen] = useState(false)
@@ -55,78 +68,56 @@ function RegisterForm() {
         setLoginDetails({ ...prevState })
     }
 
-    var toastNotification={
-        title: "Note:",
-        message: "",
-        type: "danger",
-        insert: "bottom",
-        container: "top-right",
-        animationIn: ["animate__animated", "animate__fadeIn"],
-        animationOut: ["animate__animated", "animate__fadeOut"],
-        dismiss: {
-            duration: 4000,
-            onScreen: true
-        }
-    }
-
     const clickedSubmit = async () => {
-  
-            const axios=require('axios');
- 
+        if (reCaptchaRef.current.getValue() === "") {
+            showMessage("reCaptcha verification failed", "danger")
+            return;
+        }
+        setloader(true)
+        //   console.log({...loginDetails,captcha: reCaptchaRef.current.getValue()});
 
+        const resp = await apiLogin({
+            ...loginDetails,
+            captcha: reCaptchaRef.current.getValue(),
+        });
 
+        reCaptchaRef.current.reset();
+        setloader(false)
+        if (resp === undefined) {
+            showMessage("Please try again after sometime", "danger")
+        } else {
+            if (resp.status === 200) {
+                //  setAuth(true);
+                console.log(resp.data.message);
+                showMessage("Login successful!", "success")
 
-                if (reCaptchaRef.current.getValue() === "") {
-                    // showErrorToastNotification(<p>reCaptcha verification failed</p>);
-                    return;
-                  }
-
-                //   console.log({...loginDetails,captcha: reCaptchaRef.current.getValue()});
-              
-                  const resp = await apiLogin({
-                    ...loginDetails,
-                    captcha: reCaptchaRef.current.getValue(),
-                  });
-
- 
-                  
-              
-                  reCaptchaRef.current.reset();
-              
-                  if (resp === undefined) {
-                    // showErrorToastNotification(<p>Please try again after sometime</p>);
-                  } else {
-                    if (resp.status === 200) {
-                        //  setAuth(true);
-                        console.log(resp.data.message);
-                        Store.addNotification({...toastNotification,message:resp.data.message})
-
-                    //   Success
+                //   Success
                 //       showSuccessToastNotification(<p>Logged in!</p>)
-                    //   localStorage.setItem("details", stringifyUserDetails(resp.data));
-                      localStorage.setItem("token", resp.data.token);
-                      localStorage.setItem("email", resp.data.email);
-                      localStorage.setItem("kid",resp.data.kid);
-                      localStorage.setItem("firstname",resp.data.firstname);
-                      localStorage.setItem("lastname",resp.data.lastname);
-                      localStorage.setItem("phone",resp.data.phone)
-                      localStorage.setItem("dept",resp.data.dept)
+                //   localStorage.setItem("details", stringifyUserDetails(resp.data));
+                localStorage.setItem("token", resp.data.token);
+                localStorage.setItem("email", resp.data.email);
+                localStorage.setItem("kid", resp.data.kid);
+                localStorage.setItem("firstname", resp.data.firstname);
+                localStorage.setItem("lastname", resp.data.lastname);
+                localStorage.setItem("phone", resp.data.phone)
+                localStorage.setItem("dept", resp.data.dept)
+                if (localStorage.getItem("firstTimeLogin") === null) {
+                    setIsModalOpen(true);
+                } else {
+                    navigate("/profile");
+                }
+                console.log(resp.data)
+            } else if (resp.status >= 400 && resp.status < 500) {
+                console.log(resp.data.message);
+                showMessage(resp.data.message, "danger")
+            } else if (resp.status >= 500 && resp.status < 600) {
+                console.log(resp.data.message);
+                showMessage(resp.data.message, "danger")
+            }
+        }
+        //   window.location.href="/";
 
-                      console.log(resp.data)
-                      navigate("/");
-                    } else if (resp.status >= 400 && resp.status < 500) {
-                        console.log(resp.data.message);
-                        Store.addNotification({...toastNotification,message:resp.data.message})
-                        //   showErrorToastNotification(<p>{resp.data.message}</p>);
-                    } else if (resp.status >= 500 && resp.status < 600) {
-                        console.log(resp.data.message);
-                        Store.addNotification({...toastNotification,message:resp.data.message})
-                    //   showErrorToastNotification(<p>{resp.data.message}</p>);
-                    }
-                  }
-                //   window.location.href="/";
-                return
-            }  
+    }
 
 
     if (isModalOpen) {
@@ -134,18 +125,18 @@ function RegisterForm() {
     } else {
         document.body.style.overflowY = "visible"
     }
-   
-        return (
-            <>
-                {loader && <SimpleLoader message={"Logging in"} />}
-                <div
-                    style={{ display: loader ? "none" : "flex" }}
-                    className={`${styles.formWrapper}`}
-                >
-                    <Heading text='Login' />
-                    {LOGIN_FORM_FIELDS.map((field, key) => {
-                        return (
-                            <>
+
+    return (
+        <>
+            {loader && <SimpleLoader message={"Logging in"} />}
+            <div
+                style={{ display: loader ? "none" : "flex" }}
+                className={`${styles.formWrapper}`}
+            >
+                <Heading text='Login' />
+                {LOGIN_FORM_FIELDS.map((field, key) => {
+                    return (
+                        <>
                             <FormField
                                 key={key}
                                 type={field.type}
@@ -154,30 +145,36 @@ function RegisterForm() {
                                 value={loginDetails}
                                 setter={changeLoginFormState}
                             />
-                   
-                            </>
-                        );
-                    })}
-                        <ReCAPTCHA
-                            sitekey={"6LcMoTUdAAAAAGFo2lgEFl5sIpitgdT-lExG05FL"}
-                            theme="dark"
-                            size="normal"
-                            className="recaptcha"
-                            ref={reCaptchaRef}
-                          />
-                    <div>
-                        <Button text={"Login"} onClickMethod={ clickedSubmit } />
-                    </div>
-                    <button
-                        className={styles.btn_s}
-                         onClick={() => window.open("https://kurukshetraceg.org.in/register")}
-                     >
-                        Sign Up for K!
-                    </button>
 
+                        </>
+                    );
+                })}
+                <ReCAPTCHA
+                    sitekey={"6LcMoTUdAAAAAGFo2lgEFl5sIpitgdT-lExG05FL"}
+                    theme="dark"
+                    size="normal"
+                    className="recaptcha"
+                    ref={reCaptchaRef}
+                />
+                <div>
+                    <Button text={"Login"} onClickMethod={clickedSubmit} />
+                    <Modal showCloseIcon={false} open={isModalOpen} onClose={() => { setIsModalOpen(false) }} center autofocus={false} classNames={{
+                        overlay: `${styles.customOverlay}`,
+                        modal: `${styles.customModal}`,
+                    }}>
+                        <Popup />
+                    </Modal>
                 </div>
-            </>
-        );
+                <button
+                    className={styles.btn_s}
+                    onClick={() => window.open("https://kurukshetraceg.org.in/register")}
+                >
+                    Sign Up for K!
+                </button>
+
+            </div>
+        </>
+    );
 }
 
 export default RegisterForm;
