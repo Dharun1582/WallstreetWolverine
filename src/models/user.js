c05d5b4660bf1754716fafc66a07859c9a708a5a
Vaalarivan2002 <pvaalarivan@gmail.com>
2022-03-28 22:47:59 +0530

    google icon fix

---
+++ src/models/user.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class user extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    user.init({
+        id: DataTypes.UUID,
+        kid: DataTypes.INTEGER,
+        firstname: DataTypes.STRING,
+        lastname: DataTypes.STRING,
+        email: {
+            type: DataTypes.STRING,
+            primaryKey: true
+        },
+        phone: DataTypes.STRING,
+        college: DataTypes.STRING,
+        year: DataTypes.INTEGER,
+        dept: DataTypes.STRING,
+        pwdhash: DataTypes.STRING,
+        cegian: DataTypes.BOOLEAN,
+        salt: DataTypes.STRING,
+        vsalt: DataTypes.STRING,
+        roll: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'user',
+    });
+    return user;
+};
---
+++ src/pages/Test/Test.js
@@ -0,0 +1,10 @@
+import React, { useState, createContext, useEffect, useRef } from "react";
+
+import styles from './Test.module.css'
+function Test() {
+    return (
+        <img src="assets/images/google.png" alt="" className={styles.googleIcon}/>
+    )
+}
+
+export default Test
