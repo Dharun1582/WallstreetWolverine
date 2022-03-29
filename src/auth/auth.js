193609f0897120d16300c759dafeac6998997cae
wolf <palanivaradharaj7@gmail.com>
2022-03-29 18:58:27 +0530

    contact-api-call

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -20,102 +20,99 @@ export const apiFetchGraphData = async () => {
   }
 };
 
-export const apiPostRegisterDetails = async (details,config) => {
-    try {
-      const response = await api.post("register",details,config);
-      return response;
-    } catch (error) {
-      return error.response;
-    }
-  };
+export const apiPostRegisterDetails = async (details, config) => {
+  try {
+    const response = await api.post("register", details, config);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 
-  export const apiGetWallet = async (config) => {
-    try {
-      const response = await api.get("stock",config);
-      return response;
-    } catch (error) {
-      return error.response;
-    }
-  };
+export const apiGetWallet = async (config) => {
+  try {
+    const response = await api.get("stock", config);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 
-  export const apiGetHistory = async (config) => {
-    try {
-      const response = await api.get("history",config);
-      return response;
-    } catch (error) {
-      return error.response;
-    }
-  };
-  
-  export const apicheckUser = async (config) => {
-    try {
-      const response = await api.get("checkUser",config);
-      return response;
-    } catch (error) {
-      return error.response;
-    }
-  };
+export const apiGetHistory = async (config) => {
+  try {
+    const response = await api.get("history", config);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 
-  export const apicheckRecord = async (config) => {
-    try {
-      const response = await api.get("checkRecord",config);
-      return response;
-    } catch (error) {
-      return error.response;
-    }
-  };
+export const apicheckUser = async (config) => {
+  try {
+    const response = await api.get("checkUser", config);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 
-  export const apigetProfile = async (config) => {
-    try {
-      const response = await api.get("profile",config);
-      return response;
-    } catch (error) {
-      return error.response;
-    }
-  };
+export const apicheckRecord = async (config) => {
+  try {
+    const response = await api.get("checkRecord", config);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 
-  export const apiBuyStock = async (column, value,nos,config) => {
-    try {
-      const response = await api.get(`buyStock/${column}/${value}/${nos}`,config);
-      return response;
-    } catch (error) {
-      return error.response;
-    }
-  };
+export const apigetProfile = async (config) => {
+  try {
+    const response = await api.get("profile", config);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 
-  export const apiSellStock = async (column, value,nos,config) => {
-    try {
-      const response = await api.get(`sellStock/${column}/${value}/${nos}`,config);
-      return response;
-    } catch (error) {
-      return error.response;
-    }
-  };
+export const apiBuyStock = async (column, value, nos, config) => {
+  try {
+    const response = await api.get(`buyStock/${column}/${value}/${nos}`, config);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+const url_contact = "api/user/query";
+
+export const apiSellStock = async (column, value, nos, config) => {
+  try {
+    const response = await api.get(`sellStock/${column}/${value}/${nos}`, config);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 
 
 export const apisendMail = async (data) => {
-    try {
-      const response = await api.post(url_sendMail, {
-        email: data.email,
-        name: data.name,
-        message: data.message,
-      });
-      // console.log(response.data);
-      return response;
-    } catch (error) {
-      // console.log(error.response);
-      return error.response;
-    }
-  };
- 
+  try {
+    const response = await api_k.post(`${url_contact}`, data);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 
-  export const apiGoogleSignin = () => {
-    // window.location = "https://www.youtube.com/" + url_googleSignin;
-    // window.location = "https://api.sherlock.kurukshetraceg.org.in/" + url_googleSignin;
-    window.location = "https://api.kurukshetraceg.org.in/" + url_googleSignin;
 
-    // window.location = "https://localhost:3000/login" + url_googleSignin;
 
-  };
-  
-  
+
+export const apiGoogleSignin = () => {
+  // window.location = "https://www.youtube.com/" + url_googleSignin;
+  // window.location = "https://api.sherlock.kurukshetraceg.org.in/" + url_googleSignin;
+  window.location = "https://api.kurukshetraceg.org.in/" + url_googleSignin;
+
+  // window.location = "https://localhost:3000/login" + url_googleSignin;
+
+};
+
